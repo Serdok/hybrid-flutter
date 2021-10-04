8c2f0bfd2a15e2a0ed86168535d611665a453d37
Anass 'Serdok' Lahnin <l.anass.pro@gmail.com>
2021-10-04 16:20:49 +0200

    Ajout starter code

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,115 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
-// Ici c'est le main de l'application
 void main() {
   runApp(const MyApp());
 }
 
-// Le widget principal, ici l'application est stateless (ne garde aucun etat en memoire)
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // Chaque widget etendu de StatelessWidget ou State<...> doit avoir cette fonction definie
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'My first Flutter app',            // Titre de l'application
-        home: Scaffold(                           // home = Route par defaut (equivalent de /)
-          appBar: AppBar(                         // Barre affichee en haut de l'ecran
-            title: const Text('Appbar title'),
-          ),
-          body: const Center(                     // Corps de l'application
-            child: RandomWords(),
-          ),
-        ));
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
   }
 }
 
-// Classe Stateful: elle garde en memoire la liste des mots generes
-class RandomWords extends StatefulWidget {
-  const RandomWords({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // Appelee a la creation de la classe
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
   @override
-  State createState() => _RandomWordsState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-// Definition d'un etat
-class _RandomWordsState extends State<RandomWords> {
-  final randomWordPairs = <WordPair>[];   // Liste des mots generes
-  final toggledWordPairs = <WordPair>{};  // Liste des mots cliques
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-  // Construction d'une ligne
-  Widget buildRow(WordPair pair) {
-    final toggled = toggledWordPairs.contains(pair);
-
-    return ListTile(                                     // ListTile = 1 element de liste
-      title: Text(pair.asLowerCase,
-          style: const TextStyle(
-              fontSize: 29,
-              fontFamily: 'Roboto',
-              color: Colors.cyan)
-      ),
-      trailing: Icon(                                     // Trailing = a la fin
-        Icons.access_alarm,
-        color: toggled ? Colors.red : null,
-      ),
-      onTap: () {                                         // onTap = callback appele des qu'on tape sur un element de liste
-        setState(() {
-          // Mise a jour de l'etat
-          toggled ? toggledWordPairs.remove(pair) : toggledWordPairs.add(pair);
-        });
-      },
-    );
-  }
-
-  // Construction de la liste
-  Widget buildList() {
-    return ListView.builder(                                // builder nous permet de creer une liste a la demande
-      itemBuilder: (context, item) {                        // callback qui ajoute les elements a notre liste
-        randomWordPairs.add(generateWordPairs().first);
-        return buildRow(randomWordPairs[item]);
-      },
-    );
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
-      body: buildList(),
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
